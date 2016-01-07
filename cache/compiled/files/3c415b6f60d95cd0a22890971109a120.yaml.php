5edb902ac465f3b94e7cecc4f588a53f7df740d3
Nick Pateman <nick.pateman@certivox.com>
2016-01-07 16:34:40 +0000

    arse

--- cache/compiled/files/3c415b6f60d95cd0a22890971109a120.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'C:/wamp/www/user/config/plugins/anchors.yaml',
-    'modified' => 1450829884,
-    'data' => [
-        'selectors' => '#body h2, #body h3, #body h4, #body h5'
-    ]
-];
--- cache/compiled/files/550cbfb365abb44fe5453dcfb5a6d984.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'C:/wamp/www/user/config/plugins/highlight.yaml',
-    'modified' => 1450829884,
-    'data' => [
-        'theme' => 'learn'
-    ]
-];
